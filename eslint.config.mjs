302668dad2ccd104fe275b81543301a2708f71be
tranquockhoa <khoatran1.tt.nd@gmail.com>
2025-08-09 11:04:43 +0700

    initial project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,16 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import js from "@eslint/js";
+
 import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: import.meta.dirname,
+  recommendedConfig: js.configs.recommended,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["eslint:recommended", "next"],
+  }),
 ];
 
 export default eslintConfig;
