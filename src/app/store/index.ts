1628a8b22bd255398b8f38b8597deb3754d5f4ee
tranquockhoa <khoatran1.tt.nd@gmail.com>
2025-10-20 16:38:32 +0700

    feat: update

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import authPopupReducer from "./auth-popup/auth-pupop.store";
 import loginRecuder from "./login/login.store";
 import profileReducer from "./profile/profile.store";
+import bannerReducer from "./banners/banner.store";
 
 export const store = configureStore({
   reducer: {
     authPopup: authPopupReducer,
     login: loginRecuder,
     profile: profileReducer,
+    banners: bannerReducer,
   },
 });
 
